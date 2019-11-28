a009af910b818b833495008ff9eef2ae686f94e7
Damir <54745928+dgalilullin@users.noreply.github.com>
2019-11-27 23:23:12 -0500

    Create 7 List all employees in the Sales and Development departments including their employee number last name first name and department name.sql

---
+++ EmployeeSQL/7 List all employees in the Sales and Development departments including their employee number last name first name and department name.sql
@@ -0,0 +1,7 @@
+SELECT emp.emp_no, emp.first_name, emp.last_name, dp.dept_name
+FROM employees as emp
+JOIN dept_emp as de
+ON emp.emp_no = de.emp_no
+JOIN departments as dp
+ON de.dept_no = dp.dept_no
+WHERE dept_name = 'Sales' OR dept_name = 'Development'
